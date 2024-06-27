ca25f1bd77608ee207dbeed988389e18de82e1d2
edlugz <eddy.lugaye@gmail.com>
2024-06-27 13:17:54 +0300

    update migrations

--- src/database/migrations/2024_06_26_094501_create_mpesa_transactions_table.php
+++ src/database/migrations/2024_06_26_094501_create_mpesa_transactions_table.php
@@ -21,10 +21,21 @@ return new class() extends Migration {
             $table->integer('bill_reference')->nullable();
             $table->string('requester_name')->nullable();
             $table->string('requester_mobile')->nullable();
+            $table->string('conversation_id')->nullable();
+            $table->string('originator_conversation_id')->nullable();
+            $table->string('response_code')->nullable();
+            $table->string('response_description')->nullable();
+            $table->string('result_type')->nullable();
+            $table->string('result_code')->nullable();
+            $table->string('result_description')->nullable();
+            $table->string('transaction_id')->nullable();
+            $table->timestamp('transaction_completed_date_time')->nullable();
+            $table->string('receiver_party_public_name')->nullable();
             $table->json('json_request')->nullable();
             $table->json('json_response')->nullable();
             $table->json('json_result')->nullable();
             $table->timestamps();
+			$table->softDeletes();
         });
     }
 
---
+++ src/database/migrations/2024_06_26_154356_create_mpesa_fundings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mpesa_fundings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('fund_id');
+            $table->string('mobile_no');
+            $table->string('amount');
+            $table->string('bill_reference');
+            $table->string('merchant_request_id')->nullable();
+            $table->string('checkout_request_id')->nullable();
+            $table->string('response_code')->nullable();
+            $table->string('response_description')->nullable();
+            $table->string('result_code')->nullable();
+            $table->string('result_desc')->nullable();
+            $table->string('mpesa_receipt_number')->nullable();
+            $table->string('transaction_date')->nullable();
+            $table->string('json_request')->nullable();
+            $table->string('json_response')->nullable();
+            $table->string('json_result')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mpesa_fundings');
+    }
+};
