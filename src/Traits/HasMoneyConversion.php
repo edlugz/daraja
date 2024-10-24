e6acf0ab3efbc3119ff7cff2e88b9329a9a5b5e0
edlugz <eddy.lugaye@gmail.com>
2024-10-25 00:52:31 +0300

    Create HasMoneyConversion.php
    - add money conversion trait

---
+++ src/Traits/HasMoneyConversion.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EdLugz\Daraja\Traits;
+
+trait HasMoneyConversion
+{
+    /**
+     * From Float Currency to Cents
+     *
+     * Multiplies the value by 100 so that
+     * it is stored as a big integer to avoid
+     * floating points
+     *
+     * @param float $value
+     * @return int
+     */
+    private function toCents(float $value): int
+    {
+        return ($value * 100);
+    }
+
+    /**
+     * Cents currency to Float
+     *
+     * Multiplies the cents in biginteger by 0.01
+     * so that currency is retrieved correct to 2 decimal places
+     *
+     * @param int $value
+     * @return float
+     */
+    private function fromCents(int $value): float
+    {
+        return ($value * 0.01);
+    }
+}
