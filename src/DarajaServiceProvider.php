3b80a8a7e5fb95f2e7fb67bef75389b0c4d3dbd7
edlugz <eddy.lugaye@gmail.com>
2025-08-30 18:30:57 +0300

    Update DarajaServiceProvider.php

--- src/DarajaServiceProvider.php
+++ src/DarajaServiceProvider.php
@@ -13,8 +13,8 @@ final class DarajaServiceProvider extends ServiceProvider
     /**
      * Package paths.
      */
-    private const CONFIG_PATH = __DIR__ . '/../config/daraja.php';
-    private const MIGRATIONS_PATH = __DIR__ . '/../database/migrations';
+    private const string CONFIG_PATH = __DIR__ . '/../config/daraja.php';
+    private const string MIGRATIONS_PATH = __DIR__ . '/../database/migrations';
 
     /**
      * Register any application services.
