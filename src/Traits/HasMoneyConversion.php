c2c0fc1ed43c834130008a2d5fc03b848ce5fcfb
Henriquedn <henrydkm@gmail.com>
2025-09-06 19:15:42 +0300

    Fix Money Conversion toCents <=> fromCents

--- src/Traits/HasMoneyConversion.php
+++ src/Traits/HasMoneyConversion.php
@@ -6,32 +6,13 @@ namespace EdLugz\Daraja\Traits;
 
 trait HasMoneyConversion
 {
-    /**
-     * From Float Currency to Cents
-     *
-     * Multiplies the value by 100 so that
-     * it is stored as a big integer to avoid
-     * floating points
-     *
-     * @param float $value
-     * @return float
-     */
-    private function toCents(float $value): float
+    private function toCents(float $value): int
     {
-        return ($value * 100);
+        return intval(round($value * 100));
     }
 
-    /**
-     * Cents currency to Float
-     *
-     * Multiplies the cents in biginteger by 0.01
-     * so that currency is retrieved correct to 2 decimal places
-     *
-     * @param int $value
-     * @return float
-     */
     private function fromCents(int $value): float
     {
-        return ($value * 0.01);
+        return round($value / 100, 2);
     }
 }
